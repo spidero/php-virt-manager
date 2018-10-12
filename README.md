18318e280b862a66b3dd3e39bcd28cf794aa62b7
spidero <piotr@skwarna.eu>
2018-10-12 02:49:10 -0400

    add files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # php-virt-manager
+
+Requirements:
+- php min. 5.2
+- php-composer
+- php-libvirt
+
+Installation:
+
+composer install
+mkdir -p {templates_c,configs,cache}
--- function.php
+++ function.php
@@ -1,5 +1,13 @@
 <?php
 
 require_once('config.php');
+require_once('menu.php');
+
+$smarty->assign('box_info', "");
+
+function xpath_return($res, $path) {
+  $x = libvirt_domain_xml_xpath($res, $path);
+  return $x[0];
+}
 
 ?>
