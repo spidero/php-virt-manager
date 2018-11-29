b9cc07b0a09e3aebebbcf6a1c42eaeb2eb2f320a
spidero <piotr@skwarna.eu>
2018-11-29 07:14:28 -0500

    - remove echo from nodes - started storage information

--- node.php
+++ node.php
@@ -7,12 +7,12 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
 	$state	= filter_input(INPUT_GET, 'state',  FILTER_SANITIZE_STRING);
 //	echo "$node|$state";
 
-	
+
 	if ($state=="start"){
         $res  = libvirt_domain_lookup_by_name ($con, $node);
         $info = libvirt_domain_create($res);
         if ($info==1){
-            $box_info = 
+            $box_info =
             '
                     <div class="card text-white bg-success mb-3" style="max-width: 100%;">
                         <div class="card-header">Info</div>
@@ -24,7 +24,7 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
             $smarty->assign('box_info', $box_info);
         }
         else {
-                    $box_info = 
+                    $box_info =
             '
                     <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
                         <div class="card-header">Info</div>
@@ -39,9 +39,9 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
 	elseif ($state=="stop"){
 	    $res  = libvirt_domain_lookup_by_name ($con, $node);
         $info = libvirt_domain_shutdown($res);
-        
+
         if ($info==1){
-            $box_info = 
+            $box_info =
             '
                     <div class="card text-white bg-success mb-3" style="max-width: 100%;">
                         <div class="card-header">Info</div>
@@ -53,7 +53,7 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
             $smarty->assign('box_info', $box_info);
         }
         else {
-                    $box_info = 
+                    $box_info =
             '
                     <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
                         <div class="card-header">Info</div>
@@ -69,7 +69,7 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
 	    $res  = libvirt_domain_lookup_by_name ($con, $node);
         $info = libvirt_domain_reboot($res);
         if ($info==1){
-            $box_info = 
+            $box_info =
             '
                     <div class="card text-white bg-success mb-3" style="max-width: 100%;">
                         <div class="card-header">Info</div>
@@ -81,7 +81,7 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
             $smarty->assign('box_info', $box_info);
         }
         else {
-                    $box_info = 
+                    $box_info =
             '
                     <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
                         <div class="card-header">Info</div>
@@ -93,12 +93,12 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
             $smarty->assign('box_info', $box_info);
         }
 	}
-	
+
 	elseif ($state=="suspend"){
 	    $res  = libvirt_domain_lookup_by_name ($con, $node);
         $info = libvirt_domain_suspend($res);
         if ($info==1){
-            $box_info = 
+            $box_info =
             '
                     <div class="card text-white bg-success mb-3" style="max-width: 100%;">
                         <div class="card-header">Info</div>
@@ -110,64 +110,7 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
             $smarty->assign('box_info', $box_info);
         }
         else {
-                    $box_info = 
-            '
-                    <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
-                        <div class="card-header">Info</div>
-                            <div class="card-body">
-                                <h5 class="card-title">Error: '.$info.'</h5>
-                        </div>
-                    </div>
-            ';
-            $smarty->assign('box_info', $box_info);
-        }
-	}	
-	elseif ($state=="resume"){
-	    $res  = libvirt_domain_lookup_by_name ($con, $node);
-        $info = libvirt_domain_resume($res);
-        if ($info==1){
-            $box_info = 
-            '
-                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
-                        <div class="card-header">Info</div>
-                            <div class="card-body">
-                                <h5 class="card-title">Resuming machine, it may take some time</h5>
-                        </div>
-                    </div>
-            ';
-            $smarty->assign('box_info', $box_info);
-        }
-        else {
-                    $box_info = 
-            '
-                    <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
-                        <div class="card-header">Info</div>
-                            <div class="card-body">
-                                <h5 class="card-title">Error: '.$info.'</h5>
-                        </div>
-                    </div>
-            ';
-            $smarty->assign('box_info', $box_info);
-        }
-	}	
-	
-		elseif ($state=="destroy"){
-		$res  = libvirt_domain_lookup_by_name ($con, $node);
-        $info = libvirt_domain_destroy($res);
-        if ($info==1){
-            $box_info = 
-            '
-                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
-                        <div class="card-header">Info</div>
-                            <div class="card-body">
-                                <h5 class="card-title">Destroying machine, it may take some time</h5>
-                        </div>
-                    </div>
-            ';
-            $smarty->assign('box_info', $box_info);
-        }
-        else {
-                    $box_info = 
+                    $box_info =
             '
                     <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
                         <div class="card-header">Info</div>
@@ -179,26 +122,83 @@ if (isset($_GET['node']) AND isset($_GET['state'])){
             $smarty->assign('box_info', $box_info);
         }
 	}
-	
+	elseif ($state=="resume"){
+	    $res  = libvirt_domain_lookup_by_name ($con, $node);
+        $info = libvirt_domain_resume($res);
+        if ($info==1){
+            $box_info =
+            '
+                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                        <div class="card-header">Info</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Resuming machine, it may take some time</h5>
+                        </div>
+                    </div>
+            ';
+            $smarty->assign('box_info', $box_info);
+        }
+        else {
+                    $box_info =
+            '
+                    <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
+                        <div class="card-header">Info</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Error: '.$info.'</h5>
+                        </div>
+                    </div>
+            ';
+            $smarty->assign('box_info', $box_info);
+        }
+	}
+
+		elseif ($state=="destroy"){
+		$res  = libvirt_domain_lookup_by_name ($con, $node);
+        $info = libvirt_domain_destroy($res);
+        if ($info==1){
+            $box_info =
+            '
+                    <div class="card text-white bg-success mb-3" style="max-width: 100%;">
+                        <div class="card-header">Info</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Destroying machine, it may take some time</h5>
+                        </div>
+                    </div>
+            ';
+            $smarty->assign('box_info', $box_info);
+        }
+        else {
+                    $box_info =
+            '
+                    <div class="card text-white bg-danger mb-3" style="max-width: 100%;">
+                        <div class="card-header">Info</div>
+                            <div class="card-body">
+                                <h5 class="card-title">Error: '.$info.'</h5>
+                        </div>
+                    </div>
+            ';
+            $smarty->assign('box_info', $box_info);
+        }
+	}
+
 }
 
 if (isset($_GET['node'])){
 	$node	= filter_input(INPUT_GET, 'node',  FILTER_SANITIZE_STRING);
-	
-	///////////
+
+/*	///////////
 echo "-$node-";
 $res = libvirt_domain_lookup_by_name ($con, $node);
 $ss = libvirt_domain_get_screenshot($res, $node, 1);
 print_r($ss);
 echo "-";
 ///////////
-	
+	*/
     $smarty->assign('node', $node);
     //print_r( libvirt_domain_get_info (libvirt_domain_lookup_by_name ($con, $node)) );
     //echo "<hr>";
-    
+
     $res = libvirt_domain_lookup_by_name ($con, $node);
-    
+
     $domain_uuid = libvirt_domain_get_uuid_string ($res);
     $smarty->assign('domain_uuid', $domain_uuid);
 
@@ -206,29 +206,29 @@ echo "-";
     $smarty->assign('active', $active);
 
     $info = libvirt_domain_get_info($res);
-    $info['maxMem'] = round($info['maxMem']/(1024*1024)); 
-    $info['memory'] = round($info['memory']/(1024*1024)); 
+    $info['maxMem'] = round($info['maxMem']/(1024*1024));
+    $info['memory'] = round($info['memory']/(1024*1024));
     $smarty->assign('info', $info);
-    
+
     if ($info['state']==1){
         $vnc_ip = xpath_return($res, '//domain/devices/graphics/@listen');
         $smarty->assign('vnc_ip', $vnc_ip);
-        
+
         $vnc_port = xpath_return($res, '//domain/devices/graphics/@port');
         $smarty->assign('vnc_port', $vnc_port);
-        
+
         //print_r(xpath_return($res, '//domain/devices/disk[@device="disk"]/target/@bus', false));
         //print_r( libvirt_domain_xml_xpath($res, '//domain/devices/disk[@device="disk"]/target/@bus', false));
-        
+
         //echo "<br>";
         //print_r(xpath_return($res, '//domain/devices/disk[@device="disk"]/target/@dev', false));
         //print_r( libvirt_domain_xml_xpath($res, '//domain/devices/disk[@device="disk"]/target/@dev', false));
-        
+
         //echo "<br>[";
         //print_r(libvirt_domain_get_block_info('$res', 'hda'));
         //print_r(libvirt_domain_get_autostart('$res'));
         //echo "]<br>";
-        
+
     }
 
 }
--- templates/storage.tpl
+++ templates/storage.tpl
@@ -1,6 +1,24 @@
 {include file="header.tpl"}
 <div class="card">
   <div class="card-body">
-Storage
+
+{if $storage==""}
+Storage pools:<br><br>
+{foreach from=$storagepools item=sp}
+<b><a href="storage.php?storage={$sp}">{$sp}</a></b><br>
+{/foreach}
+{else}
+Storage <b>{$storage}</b> details:
+<hr>
+{$xml}
+<hr>
+<p>Files</p>
+
+{foreach from=$list item=l}
+{$l}<br>
+{/foreach}
+{/if}
+
+  </div>
 </div>
 {include file="footer.tpl"}
